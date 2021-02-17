b14f4ae793f1a442da781cbd970bdbb8212cb87c
raki <rakeshhotker@gmail.com>
2021-02-17 07:10:04 +0530

    setting up views

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const PORT = 8000;
 //use express router
 app.use('/', require('./routes'));
+//set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 app.listen(PORT, function (err) {
   if (!err) {
     console.log(`Server running on PORT:${PORT}`);
